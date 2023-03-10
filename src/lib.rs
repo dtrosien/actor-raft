4e2de306c21f83663c27376b7e88ff229805e953
dtrosien <dtrosien@gmail.com>
2023-03-10 18:39:06 +0100

    sled db implementation

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ mod actors;
 mod config;
 pub mod raft;
 mod rpc;
+mod db;
+
 mod raft_rpc {
     tonic::include_proto!("raft_proto"); // The string specified here must match the proto package name
 }
