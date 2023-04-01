1f503041b5551c28f2ede5eea99bf1fb6374b39a
dtrosien <dtrosien@gmail.com>
2023-04-01 19:14:41 +0200

    implementation of append_entries rpc client with tests

--- src/rpc/client.rs
+++ src/rpc/client.rs
@@ -25,22 +25,33 @@ pub async fn request_vote(
 
     let response = client.request_votes(request).await?;
     let response_arguments = response.into_inner();
-    println!("vote granted:{}", response_arguments.vote_granted);
+    println!("vote granted: {}", response_arguments.vote_granted);
     Ok(Reply {
         term: response_arguments.term,
         success_or_granted: response_arguments.vote_granted,
     })
 }
 
-async fn append_entry(
+pub async fn append_entry(
     uri: String,
     append_entry_request: AppendEntriesRequest,
-) -> Result<Reply, Box<dyn Error>> {
-    //todo implement append_entry client
+) -> Result<Reply, Box<dyn Error + Send + Sync>> {
+    let channel = Channel::builder(uri.parse()?)
+        .connect_timeout(Duration::from_millis(2)) // todo not needed?
+        .connect()
+        .await?;
+
+    let request = tonic::Request::new(append_entry_request);
+    //let timeout_channel = Timeout::new(channel, Duration::from_secs(10)); //todo define timeout for answer?
+    let mut client = RaftRpcClient::new(channel);
+
+    let response = client.append_entries(request).await?;
+    let response_arguments = response.into_inner();
+    println!("replicated: {}", response_arguments.success);
 
     Ok(Reply {
-        term: 0,
-        success_or_granted: true,
+        term: response_arguments.term,
+        success_or_granted: response_arguments.success,
     })
 }
 
@@ -130,6 +141,100 @@ mod tests {
         let serve_future = async { start_test_server(port, TestServerFalse {}).await };
         let request_future =
             async { start_test_request(|| request_vote(uri.clone(), vote_request.clone())).await };
+
+        tokio::select! {
+                _ = serve_future => panic!("server returned first"),
+                r = request_future => {
+                    if let Ok(_r) = r {panic!("must return Error")
+                    }
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn append_entries_true_test() {
+        let append_entries_request = AppendEntriesRequest {
+            term: 0,
+            leader_id: 0,
+            prev_log_index: 0,
+            prev_log_term: 0,
+            entries: vec![],
+            leader_commit: 0,
+        };
+        // ports need to be different for each test case since async
+        let port = get_test_port().await;
+        let uri = format!("https://[::1]:{port}");
+
+        let serve_future = async { start_test_server(port, TestServerTrue {}).await };
+        let request_future = async {
+            start_test_request(|| append_entry(uri.clone(), append_entries_request.clone())).await
+        };
+
+        tokio::select! {
+                _ = serve_future => panic!("server returned first"),
+                r = request_future => {
+                    match r {
+                        Ok(r) => {
+                                    assert!(r.success_or_granted);
+                                    assert_eq!(r.term,1)
+                    },
+                        _ => panic!("must not return Error")
+                    }
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn append_entries_false_test() {
+        let append_entries_request = AppendEntriesRequest {
+            term: 0,
+            leader_id: 0,
+            prev_log_index: 0,
+            prev_log_term: 0,
+            entries: vec![],
+            leader_commit: 0,
+        };
+        // ports need to be different for each test case since async
+        let port = get_test_port().await;
+        let uri = format!("https://[::1]:{port}");
+
+        let serve_future = async { start_test_server(port, TestServerFalse {}).await };
+        let request_future = async {
+            start_test_request(|| append_entry(uri.clone(), append_entries_request.clone())).await
+        };
+
+        tokio::select! {
+                _ = serve_future => panic!("server returned first"),
+                r = request_future => {
+                    match r {
+                        Ok(r) => {
+                                    assert!(!r.success_or_granted);
+                                    assert_eq!(r.term,1)
+                    },
+                        _ => panic!("must not return Error")
+                    }
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn append_entries_error_test() {
+        let append_entries_request = AppendEntriesRequest {
+            term: 0,
+            leader_id: 0,
+            prev_log_index: 0,
+            prev_log_term: 0,
+            entries: vec![],
+            leader_commit: 0,
+        };
+        // ports need to be different for each test case since async
+        let port = get_test_port().await;
+        let uri = "caseMustPanic".to_string();
+
+        let serve_future = async { start_test_server(port, TestServerFalse {}).await };
+        let request_future = async {
+            start_test_request(|| append_entry(uri.clone(), append_entries_request.clone())).await
+        };
 
         tokio::select! {
                 _ = serve_future => panic!("server returned first"),
