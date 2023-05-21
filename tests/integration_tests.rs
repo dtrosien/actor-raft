fafa5d4ebd3382546ae3616d79eebb49b6f82d8b
dtrosien <dtrosien@gmail.com>
2023-05-21 16:35:13 +0200

    refactoring = start implementing failover int test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,12 +1,13 @@
 use crate::common::{enable_tracing, get_test_db_paths, get_test_port, IntegrationTestApp};
-use actor_raft::raft_server::config::NodeConfig;
+use actor_raft::raft_server::config::{Config, NodeConfig};
 use actor_raft::raft_server::raft_handles::RaftHandles;
-use actor_raft::raft_server::raft_node::ServerState::{Candidate, Leader};
+use actor_raft::raft_server::raft_node::ServerState::{Candidate, Follower, Leader};
 use actor_raft::raft_server::raft_node::{RaftNode, RaftNodeBuilder};
+use std::collections::VecDeque;
 use std::time::Duration;
 use tokio::sync::broadcast;
 use tokio::sync::broadcast::Sender;
-use tracing::{debug, info};
+use tracing::info;
 
 mod common;
 
@@ -16,7 +17,7 @@ async fn election_test() {
 
     // prepare nodes
 
-    let (mut nodes, handles, _s_shutdown) = prepare_cluster(3).await;
+    let (mut nodes, handles, _shutdown_receivers) = prepare_cluster(3, true).await;
 
     let mut raft_node3 = nodes.pop().unwrap();
     let mut raft_node2 = nodes.pop().unwrap();
@@ -54,7 +55,7 @@ async fn replication_test() {
 
     // prepare nodes
 
-    let (mut nodes, mut handles, s_shutdown) = prepare_cluster(3).await;
+    let (mut nodes, mut handles, shutdown_receivers) = prepare_cluster(3, true).await;
 
     let mut raft_node3 = nodes.pop().unwrap();
     let mut raft_node2 = nodes.pop().unwrap();
@@ -84,7 +85,10 @@ async fn replication_test() {
 
     let t4 = tokio::spawn(async move {
         tokio::time::sleep(Duration::from_millis(1000)).await;
-        s_shutdown.send(()).unwrap();
+        // shutdown all nodes
+        shutdown_receivers.iter().for_each(|x| {
+            x.send(()).unwrap();
+        });
     });
 
     // create entry in first node (first node from prepare cluster is expected to be leader)
@@ -126,7 +130,7 @@ async fn failover_test() {
 
     // prepare test
 
-    let (mut nodes, mut handles, s_shutdown) = prepare_cluster(3).await;
+    let (mut nodes, mut handles, shutdown_receivers) = prepare_cluster(3, true).await;
 
     let mut raft_node3 = nodes.pop().unwrap();
     let mut raft_node2 = nodes.pop().unwrap();
@@ -140,6 +144,10 @@ async fn failover_test() {
     let s2 = raft_node2.get_node_server_handle().unwrap();
     let s3 = raft_node3.get_node_server_handle().unwrap();
 
+    let node1_config = raft_node1.get_config();
+    let s_shutdown: Sender<()> = broadcast::channel(1).0;
+
+    let mut shutdown_receivers_clone = shutdown_receivers.clone();
     // begin test
     for h in handles.clone() {
         info!(
@@ -168,15 +176,27 @@ async fn failover_test() {
 
     let t4 = tokio::spawn(async move {
         tokio::time::sleep(Duration::from_millis(1000)).await;
-        s_shutdown.send(()).unwrap();
+        shutdown_receivers.iter().for_each(|x| {
+            let _ = x.send(());
+        });
     });
 
+    // replicate one entry from node 1
     let t5 = tokio::spawn(async move {
         tokio::time::sleep(Duration::from_millis(400)).await;
         let entry = r_handle1.create_entry("test".to_string()).await.unwrap();
         r_handle1.append_entry(entry).await;
         let index = r_handle1.log_store.get_last_log_index().await;
-        info!("log index: {}", index);
+        assert_eq!(index, 1);
+
+        shutdown_receivers_clone
+            .pop_back()
+            .unwrap()
+            .send(())
+            .unwrap();
+        tokio::time::sleep(Duration::from_millis(500)).await;
+
+        // let failover_node = prepare_node_from_config(node1_config, s_shutdown).await;
     });
 
     tokio::try_join!(s1, s2, s3, t1, t2, t3, t4, t5).unwrap();
@@ -195,16 +215,20 @@ async fn failover_test() {
     }
 }
 
-// first node (id=0) will be leader after first run iteration. (this will be the last node to pop from Vec!)
-async fn prepare_cluster(num_nodes: u16) -> (Vec<RaftNode>, Vec<RaftHandles>, Sender<()>) {
+// first node (id=0) will be leader after first run iteration when enabled. (this will be the last node to pop from Vec!)
+async fn prepare_cluster(
+    num_nodes: u16,
+    predetermined_leader: bool,
+) -> (Vec<RaftNode>, Vec<RaftHandles>, VecDeque<Sender<()>>) {
     let mut ports: Vec<u16> = Vec::new();
     let mut node_configs: Vec<NodeConfig> = Vec::new();
     let mut raft_nodes: Vec<RaftNode> = Vec::new();
     let mut handles: Vec<RaftHandles> = Vec::new();
-    let s_shutdown = broadcast::channel(1).0;
     let mut db_paths = get_test_db_paths(3 * num_nodes).await;
+    let mut shutdown_receivers: VecDeque<Sender<()>> = VecDeque::new();
 
     for i in 0..num_nodes {
+        let s_shutdown = broadcast::channel(1).0;
         let port = get_test_port().await;
         let node_config = NodeConfig {
             id: i as u64,
@@ -213,7 +237,11 @@ async fn prepare_cluster(num_nodes: u16) -> (Vec<RaftNode>, Vec<RaftHandles>, Se
         };
         ports.push(port);
         node_configs.push(node_config);
+        shutdown_receivers.push_front(s_shutdown);
     }
+
+    // clone receivers to be able to return them later
+    let mut copy_of_receivers = shutdown_receivers.clone();
 
     // prepare raft nodes
     for i in 0..num_nodes {
@@ -222,11 +250,11 @@ async fn prepare_cluster(num_nodes: u16) -> (Vec<RaftNode>, Vec<RaftHandles>, Se
         let mut other_nodes = node_configs.clone();
         other_nodes.remove(i as usize);
 
-        let raft_node = if i == 0 {
+        let raft_node = if i == 0 && predetermined_leader {
             RaftNodeBuilder::new(app)
                 .with_id(i as u64)
                 .with_port(*ports.get(i as usize).unwrap())
-                .with_shutdown(s_shutdown.clone())
+                .with_shutdown(copy_of_receivers.pop_back().unwrap())
                 .with_client_service_enabled(false)
                 .with_nodes(other_nodes)
                 .with_log_db_path(db_paths.pop().unwrap().as_str())
@@ -239,7 +267,7 @@ async fn prepare_cluster(num_nodes: u16) -> (Vec<RaftNode>, Vec<RaftHandles>, Se
             RaftNodeBuilder::new(app)
                 .with_id(i as u64)
                 .with_port(*ports.get(i as usize).unwrap())
-                .with_shutdown(s_shutdown.clone())
+                .with_shutdown(copy_of_receivers.pop_back().unwrap())
                 .with_client_service_enabled(false)
                 .with_nodes(other_nodes)
                 .with_log_db_path(db_paths.pop().unwrap().as_str())
@@ -259,5 +287,21 @@ async fn prepare_cluster(num_nodes: u16) -> (Vec<RaftNode>, Vec<RaftHandles>, Se
         h.initiator.reset_voted_for().await;
     }
 
-    (raft_nodes, handles, s_shutdown)
+    (raft_nodes, handles, shutdown_receivers)
+}
+
+async fn prepare_node_from_config(config: Config, s_shutdown: Sender<()>) -> RaftNode {
+    let app = Box::new(IntegrationTestApp {});
+    RaftNodeBuilder::new(app)
+        .with_id(config.id)
+        .with_port(config.port)
+        .with_shutdown(s_shutdown)
+        .with_client_service_enabled(false)
+        .with_nodes(config.nodes)
+        .with_log_db_path(config.log_db_path.as_str())
+        .with_term_db_path(config.term_db_path.as_str())
+        .with_vote_db_path(config.vote_db_path.as_str())
+        .with_initial_state(Follower)
+        .build()
+        .await
 }
